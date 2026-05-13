d7400c87188b12cd357fdb5e0ddbece1301510ef
ahmadumer17 <ahmadumer1701@gmail.com>
2026-05-14 03:36:26 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    allowedHosts: true, // ✅ This tells Vite to allow Localtunnel!
+    allowedHosts: true, // ✅ Our Localtunnel fix
+    proxy: {
+      // ✅ This tells React to send API requests to your FastAPI backend!
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      }
+    }
   }
 })
